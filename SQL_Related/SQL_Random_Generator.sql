078884c8fa4d6dab7f77b4aafb3a2c4c33e93cf1
nstuart <nstuart@uwm.com>
2026-04-06 15:48:06 -0400

    Committing & pushing for backup purposes

--- SQL_Related/SQL_Random_Generator.sql
+++ SQL_Related/SQL_Random_Generator.sql
@@ -10,3 +10,68 @@
 --PRINT CHAR(48 + ABS(CHECKSUM(NEWID())) % 80)	--returns random alpha-numeric character who's ASCII value is within the set [48, 127]
 --PRINT CRYPT_GEN_RANDOM(1)	--returns random HEX value who's length is specified via the argument -- NOTE that this argument is HOW MANY BYTES LONG to generate HEX value
 
+
+
+
+
+
+
+--https://learn.microsoft.com/en-us/sql/t-sql/functions/rand-transact-sql?view=sql-server-ver17
+
+
+
+
+--Randomly generate 0 or 1
+	SELECT ROUND(RAND(), 0) AS ZeroOrOne_01
+         , ROUND(RAND(), 0) AS ZeroOrOne_02
+         , ROUND(RAND(), 0) AS ZeroOrOne_03
+         , ROUND(RAND(), 0) AS ZeroOrOne_04
+
+
+
+--Generate random numbers greater than 1
+	DECLARE @Constant AS INT;
+	SET @Constant = 10;
+
+	SELECT @Constant * RAND() AS RandomNumber;
+
+
+
+
+
+
+--Generate random integers
+		DECLARE @Constant AS INT;
+		SET @Constant = 10;
+
+		SELECT ROUND(@Constant * RAND(), 0) AS FirstRandomInteger,
+			   FLOOR(@Constant * RAND()) AS SecondRandomInteger,
+			   CEILING(@Constant * RAND()) AS ThirdRandomInteger
+
+
+
+
+
+--Insert random values into a table
+	DECLARE @RandomTable TABLE
+		(
+			RandomIntegers INT,
+			RandomFloats FLOAT,
+			RandomBits BIT
+		);
+
+	DECLARE @RowCount AS INT;
+	DECLARE @Counter AS INT;
+
+	SET @RowCount = 10;
+	SET @Counter = 1;
+
+	WHILE @Counter <= @RowCount
+		BEGIN
+			INSERT INTO @RandomTable
+			VALUES (ROUND(10 * RAND(), 0), RAND(), CAST (ROUND(RAND(), 0) AS BIT));
+			SET @Counter = @Counter + 1;
+		END
+
+	SELECT *
+	FROM @RandomTable;
